0f4d8ad018d7839d88056b0e1f877e854bf37379
Mateus Marques <marques.m05@gmail.com>
2023-05-31 19:43:49 -0300

    Add tests for APIClient

---
+++ ShoppingCart/ShoppingCartTests/Networks/APIClient/Mocks/NetworkAdapterMock.swift
@@ -0,0 +1,21 @@
+//
+//  NetworkAdapterMock.swift
+//  ShoppingCartTests
+//
+//  Created by Mateus Marques on 31/05/23.
+//
+
+import Foundation
+@testable import ShoppingCart
+
+final class NetworkAdapterMock: NetworkAdapterProtocol {
+    var result: Result<Data>?
+    
+    func request(endpoint: any Endpoint) async -> ShoppingCart.Result<Data> {
+        if let result {
+            return result
+        } else {
+            return .failure(.undefined)
+        }
+    }
+}
